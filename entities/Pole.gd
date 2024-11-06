a3a4ad608d8744408eb76a269de8de8a1a576dbf
Jyrki Launonen <jyrki.launonen@gmail.com>
2024-11-06 16:36:23 +0200

    Fix signal values on power poles
    Since the poles didn't have a collision shape for detecting cursor
    enter/exit, and that the poles did not copy the input values, the poles
    could not be monitored.
    
    - Fix BigPole right side point offset.

--- entities/Pole.gd
+++ entities/Pole.gd
@@ -1,1 +1,7 @@
 extends CombinatorBase
+
+
+# To make poles monitorable.
+# TODO: Find out if this could be done differently without copying the values to each pole.
+func post_simulate() -> void:
+	_clear_and_copy_from_1()
