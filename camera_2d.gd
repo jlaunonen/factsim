c004095613cb1b60250a0989d1aa4144c429d4c7
Jyrki Launonen <jyrki.launonen@gmail.com>
2024-11-24 11:25:27 +0200

    Reimplement camera movement using input map
    Also the _drag_start is now the starting scene position of the
    drag/mouse, and a separate _start_offset is the starting offset of the
    camera. This way the _drag_start doesn't need to be updated each step.

--- camera_2d.gd
+++ camera_2d.gd
@@ -3,26 +3,28 @@ extends Camera2D
 
 var _is_dragging := false
 var _drag_start: Vector2
+var _start_offset: Vector2
 
-func _unhandled_input(event: InputEvent):
+
+func _input(event: InputEvent) -> void:
 	if not enabled:
+		# Don't process events from "other" layered scenes.
 		return
-	if event is InputEventMouse:
-		if event.button_mask & MOUSE_BUTTON_MASK_LEFT:
-			pass
-		elif event.button_mask & MOUSE_BUTTON_RIGHT:
-			# Not really correct position, but as used for difference,
-			# it behaves better than screen_transform.
-			var scene_pos: Vector2 = event.position / zoom.x
-			if not _is_dragging:
-				_is_dragging = true
-				_drag_start = scene_pos
-				return
 
-			var diff: Vector2 = scene_pos - _drag_start
+	if event.is_action_pressed("rmb"):
+		# Not really correct position, but as used for difference,
+		# it behaves better than screen_transform.
+		if not _is_dragging:
+			var scene_pos: Vector2 = event.position / zoom.x
+			_is_dragging = true
 			_drag_start = scene_pos
-			offset = offset - diff
+			_start_offset = offset
 			get_viewport().set_input_as_handled()
-		else:
-			_is_dragging = false
-			get_viewport().set_input_as_handled()
+	elif event.is_action_released("rmb"):
+		_is_dragging = false
+		get_viewport().set_input_as_handled()
+	elif _is_dragging and event is InputEventMouseMotion:
+		var scene_pos: Vector2 = event.position / zoom.x
+		var scene_diff: Vector2 = scene_pos - _drag_start
+		offset = _start_offset - scene_diff
+		get_viewport().set_input_as_handled()
